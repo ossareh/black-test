bec7e8a2c98b0c08d84b54f5ce322a689044431e
P. Michael Ossareh <ossareh@gmail.com>
2020-07-07 15:53:45 -0500

    need to test on github proper because act + os x + setuppython is just hard

---
+++ bad_file.py
@@ -0,0 +1,6 @@
+import os
+
+print("this is a really long line, I don't know how black is going to handle this, I'm really interested though")
+
+def some_name():
+    print(1 + 2 + 3 + 1 + 2 + 3 +3+4+5+4+3+123+123+123+123+4+89+33+344+123+3123+324234+123+234234+234234)
